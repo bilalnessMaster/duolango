d79ef7bcd66ddab7efb8cc03a5c2f485d91fd150
bilalnessmaster <jijahgiga@gmail.com>
2025-07-09 13:38:05 +0000

    01 : setup nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Barlow} from "next/font/google";
 import "./globals.css";
+import { TRPCReactProvider } from "@/trpc/client";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const barlow = Barlow({
+  subsets : ['latin'],
+  variable : "--font-barlow-sans",
+  weight : ["200" , "300" , "500" , '700']
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +20,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <TRPCReactProvider>
+      <html lang="en">
+        <body
+          className={`${barlow.variable}  antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </TRPCReactProvider>
   );
 }
